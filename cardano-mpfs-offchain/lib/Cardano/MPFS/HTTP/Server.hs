12693d03d7d31ad3d795ae7471d233daaa4c3766
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-03-12 11:13:37 +0000

    feat: add token query endpoints (root, facts, proofs, requests)

--- cardano-mpfs-offchain/lib/Cardano/MPFS/HTTP/Server.hs
+++ cardano-mpfs-offchain/lib/Cardano/MPFS/HTTP/Server.hs
@@ -14,6 +14,10 @@ module Cardano.MPFS.HTTP.Server
     , statusHandler
     , tokensHandler
     , tokenHandler
+    , tokenRootHandler
+    , tokenFactHandler
+    , tokenProofHandler
+    , tokenRequestsHandler
     ) where
 
 import Control.Monad.IO.Class (liftIO)
@@ -30,18 +34,22 @@ import Servant
 import Cardano.MPFS.Context (Context (..))
 import Cardano.MPFS.Core.Types
     ( BlockId (..)
+    , Root (..)
     , SlotNo (..)
     )
 import Cardano.MPFS.HTTP.API (API)
 import Cardano.MPFS.HTTP.Encoding (Hex (..))
 import Cardano.MPFS.HTTP.Types
-    ( StatusResponse (..)
+    ( RequestJSON
+    , StatusResponse (..)
     , TokenIdJSON (..)
     , TokenStateJSON
+    , requestToJSON
     , tokenStateToJSON
     )
 import Cardano.MPFS.Indexer qualified as Indexer
 import Cardano.MPFS.State qualified as St
+import Cardano.MPFS.Trie qualified as Trie
 
 -- | Build a WAI 'Application' from a 'Context IO'.
 mkApp :: Context IO -> Application
@@ -50,6 +58,10 @@ mkApp ctx =
         $ statusHandler ctx
             :<|> tokensHandler ctx
             :<|> tokenHandler ctx
+            :<|> tokenRootHandler ctx
+            :<|> tokenFactHandler ctx
+            :<|> tokenProofHandler ctx
+            :<|> tokenRequestsHandler ctx
 
 -- | Handler for @GET \/status@.
 statusHandler :: Context IO -> Handler StatusResponse
@@ -95,3 +107,58 @@ tokenHandler ctx (TokenIdJSON tid) = do
     case mts of
         Nothing -> throwError err404
         Just ts -> pure (tokenStateToJSON ts)
+
+-- | Handler for @GET \/tokens\/:id\/root@.
+tokenRootHandler
+    :: Context IO
+    -> TokenIdJSON
+    -> Handler Hex
+tokenRootHandler ctx (TokenIdJSON tid) =
+    liftIO
+        $ Trie.withTrie (trieManager ctx) tid
+        $ \trie -> do
+            Root r <- Trie.getRoot trie
+            pure (Hex r)
+
+-- | Handler for @GET \/tokens\/:id\/facts\/:key@.
+tokenFactHandler
+    :: Context IO
+    -> TokenIdJSON
+    -> Hex
+    -> Handler Hex
+tokenFactHandler ctx (TokenIdJSON tid) (Hex k) = do
+    mv <-
+        liftIO
+            $ Trie.withTrie (trieManager ctx) tid
+            $ \trie -> Trie.lookup trie k
+    case mv of
+        Nothing -> throwError err404
+        Just v -> pure (Hex v)
+
+-- | Handler for @GET \/tokens\/:id\/proofs\/:key@.
+tokenProofHandler
+    :: Context IO
+    -> TokenIdJSON
+    -> Hex
+    -> Handler Hex
+tokenProofHandler ctx (TokenIdJSON tid) (Hex k) = do
+    mp <-
+        liftIO
+            $ Trie.withTrie (trieManager ctx) tid
+            $ \trie -> Trie.getProof trie k
+    case mp of
+        Nothing -> throwError err404
+        Just p -> pure (Hex (Trie.unProof p))
+
+-- | Handler for @GET \/tokens\/:id\/requests@.
+tokenRequestsHandler
+    :: Context IO
+    -> TokenIdJSON
+    -> Handler [RequestJSON]
+tokenRequestsHandler ctx (TokenIdJSON tid) = do
+    reqs <-
+        liftIO
+            $ St.requestsByToken
+                (St.requests (state ctx))
+                tid
+    pure (map requestToJSON reqs)
