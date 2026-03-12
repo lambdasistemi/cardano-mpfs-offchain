6718e70d6e14b95ef483c063856137df6c3cc6f0
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-03-12 11:34:14 +0000

    feat: add POST /tx/* transaction endpoints

--- cardano-mpfs-offchain/lib/Cardano/MPFS/HTTP/Server.hs
+++ cardano-mpfs-offchain/lib/Cardano/MPFS/HTTP/Server.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module      : Cardano.MPFS.HTTP.Server
 -- Description : Servant server wiring for MPFS HTTP API
@@ -9,15 +12,6 @@
 module Cardano.MPFS.HTTP.Server
     ( -- * Application
       mkApp
-
-      -- * Handlers
-    , statusHandler
-    , tokensHandler
-    , tokenHandler
-    , tokenRootHandler
-    , tokenFactHandler
-    , tokenProofHandler
-    , tokenRequestsHandler
     ) where
 
 import Control.Monad.IO.Class (liftIO)
@@ -25,31 +19,59 @@ import Data.Proxy (Proxy (..))
 import Servant
     ( Application
     , Handler
+    , err400
     , err404
+    , err502
+    , errBody
     , serve
     , throwError
     , (:<|>) (..)
     )
 
+import Data.ByteString (ByteString)
+import Data.ByteString.Lazy.Char8 qualified as BL
+
+import Cardano.Ledger.Api.Tx (Tx)
+import Cardano.Ledger.Binary
+    ( DecoderError
+    , decodeFull'
+    , natVersion
+    , serialize'
+    )
+import Cardano.Ledger.TxIn (mkTxInPartial)
+
 import Cardano.MPFS.Context (Context (..))
 import Cardano.MPFS.Core.Types
-    ( BlockId (..)
+    ( Addr
+    , BlockId (..)
+    , ConwayEra
     , Root (..)
     , SlotNo (..)
+    , TxId
     )
 import Cardano.MPFS.HTTP.API (API)
 import Cardano.MPFS.HTTP.Encoding (Hex (..))
 import Cardano.MPFS.HTTP.Types
-    ( RequestJSON
+    ( BootRequest (..)
+    , DeleteRequest (..)
+    , EndRequest (..)
+    , InsertRequest (..)
+    , RequestJSON
+    , RetractRequest (..)
     , StatusResponse (..)
+    , SubmitRequest (..)
     , TokenIdJSON (..)
     , TokenStateJSON
+    , UpdateRequest (..)
+    , parseAddr
     , requestToJSON
     , tokenStateToJSON
     )
 import Cardano.MPFS.Indexer qualified as Indexer
 import Cardano.MPFS.State qualified as St
+import Cardano.MPFS.Submitter qualified as Sub
 import Cardano.MPFS.Trie qualified as Trie
+import Cardano.MPFS.TxBuilder qualified as Tx
 
 -- | Build a WAI 'Application' from a 'Context IO'.
 mkApp :: Context IO -> Application
@@ -62,8 +84,18 @@ mkApp ctx =
             :<|> tokenFactHandler ctx
             :<|> tokenProofHandler ctx
             :<|> tokenRequestsHandler ctx
+            :<|> txBootHandler ctx
+            :<|> txInsertHandler ctx
+            :<|> txDeleteHandler ctx
+            :<|> txUpdateHandler ctx
+            :<|> txRetractHandler ctx
+            :<|> txEndHandler ctx
+            :<|> txSubmitHandler ctx
 
--- | Handler for @GET \/status@.
+-- ---------------------------------------------------------
+-- Query handlers
+-- ---------------------------------------------------------
+
 statusHandler :: Context IO -> Handler StatusResponse
 statusHandler ctx = do
     tip <- liftIO $ Indexer.getTip (indexer ctx)
@@ -86,7 +118,6 @@ statusHandler ctx = do
                 fmap (Hex . unBlockId . snd) mcp
             }
 
--- | Handler for @GET \/tokens@.
 tokensHandler
     :: Context IO -> Handler [TokenIdJSON]
 tokensHandler ctx = do
@@ -95,7 +126,6 @@ tokensHandler ctx = do
             $ St.listTokens (St.tokens (state ctx))
     pure (map TokenIdJSON tids)
 
--- | Handler for @GET \/tokens\/:id@.
 tokenHandler
     :: Context IO
     -> TokenIdJSON
@@ -108,7 +138,6 @@ tokenHandler ctx (TokenIdJSON tid) = do
         Nothing -> throwError err404
         Just ts -> pure (tokenStateToJSON ts)
 
--- | Handler for @GET \/tokens\/:id\/root@.
 tokenRootHandler
     :: Context IO
     -> TokenIdJSON
@@ -120,7 +149,6 @@ tokenRootHandler ctx (TokenIdJSON tid) =
             Root r <- Trie.getRoot trie
             pure (Hex r)
 
--- | Handler for @GET \/tokens\/:id\/facts\/:key@.
 tokenFactHandler
     :: Context IO
     -> TokenIdJSON
@@ -135,7 +163,6 @@ tokenFactHandler ctx (TokenIdJSON tid) (Hex k) = do
         Nothing -> throwError err404
         Just v -> pure (Hex v)
 
--- | Handler for @GET \/tokens\/:id\/proofs\/:key@.
 tokenProofHandler
     :: Context IO
     -> TokenIdJSON
@@ -150,7 +177,6 @@ tokenProofHandler ctx (TokenIdJSON tid) (Hex k) = do
         Nothing -> throwError err404
         Just p -> pure (Hex (Trie.unProof p))
 
--- | Handler for @GET \/tokens\/:id\/requests@.
 tokenRequestsHandler
     :: Context IO
     -> TokenIdJSON
@@ -162,3 +188,171 @@ tokenRequestsHandler ctx (TokenIdJSON tid) = do
                 (St.requests (state ctx))
                 tid
     pure (map requestToJSON reqs)
+
+-- ---------------------------------------------------------
+-- Transaction handlers
+-- ---------------------------------------------------------
+
+-- | Serialize a 'Tx ConwayEra' to hex CBOR.
+serializeTx :: Tx ConwayEra -> Hex
+serializeTx = Hex . serialize' (natVersion @11)
+
+-- | Parse an address from hex or throw 400.
+requireAddr :: Hex -> Handler Addr
+requireAddr h =
+    case parseAddr h of
+        Right a -> pure a
+        Left msg ->
+            throwError
+                err400
+                    { errBody =
+                        BL.pack msg
+                    }
+
+txBootHandler
+    :: Context IO -> BootRequest -> Handler Hex
+txBootHandler ctx (BootRequest addrHex) = do
+    addr <- requireAddr addrHex
+    tx <-
+        liftIO $ Tx.bootToken (txBuilder ctx) addr
+    pure (serializeTx tx)
+
+txInsertHandler
+    :: Context IO -> InsertRequest -> Handler Hex
+txInsertHandler
+    ctx
+    InsertRequest
+        { irToken = TokenIdJSON tid
+        , irKey = Hex k
+        , irValue = Hex v
+        , irAddr = addrHex
+        } = do
+        addr <- requireAddr addrHex
+        tx <-
+            liftIO
+                $ Tx.requestInsert
+                    (txBuilder ctx)
+                    tid
+                    k
+                    v
+                    addr
+        pure (serializeTx tx)
+
+txDeleteHandler
+    :: Context IO -> DeleteRequest -> Handler Hex
+txDeleteHandler
+    ctx
+    DeleteRequest
+        { drToken = TokenIdJSON tid
+        , drKey = Hex k
+        , drAddr = addrHex
+        } = do
+        addr <- requireAddr addrHex
+        tx <-
+            liftIO
+                $ Tx.requestDelete
+                    (txBuilder ctx)
+                    tid
+                    k
+                    addr
+        pure (serializeTx tx)
+
+txUpdateHandler
+    :: Context IO -> UpdateRequest -> Handler Hex
+txUpdateHandler
+    ctx
+    UpdateRequest
+        { urToken = TokenIdJSON tid
+        , urAddr = addrHex
+        } = do
+        addr <- requireAddr addrHex
+        tx <-
+            liftIO
+                $ Tx.updateToken
+                    (txBuilder ctx)
+                    tid
+                    addr
+        pure (serializeTx tx)
+
+txRetractHandler
+    :: Context IO -> RetractRequest -> Handler Hex
+txRetractHandler
+    ctx
+    RetractRequest
+        { rrTxId = Hex tidBytes
+        , rrTxIx = ix
+        , rrAddr = addrHex
+        } = do
+        addr <- requireAddr addrHex
+        txId <- parseTxId tidBytes
+        let txIn = mkTxInPartial txId (fromIntegral ix)
+        tx <-
+            liftIO
+                $ Tx.retractRequest
+                    (txBuilder ctx)
+                    txIn
+                    addr
+        pure (serializeTx tx)
+
+txEndHandler
+    :: Context IO -> EndRequest -> Handler Hex
+txEndHandler
+    ctx
+    EndRequest
+        { erToken = TokenIdJSON tid
+        , erAddr = addrHex
+        } = do
+        addr <- requireAddr addrHex
+        tx <-
+            liftIO
+                $ Tx.endToken
+                    (txBuilder ctx)
+                    tid
+                    addr
+        pure (serializeTx tx)
+
+txSubmitHandler
+    :: Context IO -> SubmitRequest -> Handler Hex
+txSubmitHandler ctx (SubmitRequest (Hex txCbor)) = do
+    tx <- case decodeTx txCbor of
+        Right t -> pure t
+        Left msg ->
+            throwError
+                err400
+                    { errBody =
+                        BL.pack (show msg)
+                    }
+    result <-
+        liftIO
+            $ Sub.submitTx (submitter ctx) tx
+    case result of
+        Sub.Submitted txId ->
+            pure (Hex (serialize' (natVersion @11) txId))
+        Sub.Rejected reason ->
+            throwError
+                err502
+                    { errBody =
+                        BL.fromStrict reason
+                    }
+
+-- ---------------------------------------------------------
+-- Helpers
+-- ---------------------------------------------------------
+
+-- | Decode CBOR bytes to a 'Tx ConwayEra'.
+decodeTx
+    :: ByteString
+    -> Either DecoderError (Tx ConwayEra)
+decodeTx = decodeFull' (natVersion @11)
+
+-- | Parse a 32-byte TxId from raw bytes.
+parseTxId :: ByteString -> Handler TxId
+parseTxId bs =
+    case decodeFull' (natVersion @11) bs of
+        Right tid -> pure tid
+        Left _err ->
+            throwError
+                err400
+                    { errBody =
+                        "Invalid transaction ID"
+                    }
